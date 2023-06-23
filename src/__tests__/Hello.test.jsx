0547660271a0c4d427d6e6f143d44dfa82c9b1b0
Corina Udrescu <corinutz@gmail.com>
2023-06-23 07:53:46 +0200

    Add pokemons exercise

--- src/__tests__/Hello.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Hello from "../Hello";
-
-it("renders", () => {
-  render(<Hello />);
-  expect(screen.getByText(/Hello/)).toBeInTheDocument();
-});
---
+++ src/pokemon-list.jsx
@@ -0,0 +1,7 @@
+// You can retrieve the pokemons by calling the following API
+// Make sure to replace limit and offset with the appropriate values
+// https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
+
+const PokemonList = () => {};
+
+export default PokemonList;
