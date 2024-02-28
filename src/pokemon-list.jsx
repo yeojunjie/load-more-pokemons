dd6cec2a40c2ec9e4b0dc859e9bae8a4f49a61da
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2024-02-28 17:41:53 +0800

    Implement PokemonList component

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -2,6 +2,47 @@
 // Make sure to replace limit and offset with the appropriate values
 // https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
 
-const PokemonList = () => {};
+import { useState, useEffect } from "react";
+
+const PokemonList = () => {
+
+    const [pokemons, setPokemons] = useState([]);
+    const [totalNumOfPokemons, setTotalNumOfPokemons] = useState();
+
+    async function loadMorePokemons() {
+        var offset = pokemons.length;
+
+        await fetch(`https://pokeapi.co/api/v2/pokemon?limit=5&offset=${offset}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setPokemons([...pokemons, ...data.results]);
+                setTotalNumOfPokemons(data.count);
+            })
+            .catch((error) => {
+                console.error('Error:', error)
+            });
+    }
+
+    // Call the loadMorePokemons function when the component is first mounted.
+    useEffect(() => {
+        loadMorePokemons();
+    }, []);
+
+    return (
+        <div>
+            <ol>
+                {pokemons.map((pokemon) => (
+                    <li key={pokemon.name}>{pokemon.name}</li>
+                ))}
+            </ol>
+
+            <p>
+                Displaying {pokemons.length} of {totalNumOfPokemons} results.
+            </p>
+            
+            { pokemons.length != totalNumOfPokemons && <button onClick={loadMorePokemons}>Load more</button> }
+        </div>
+    )
+};
 
 export default PokemonList;
